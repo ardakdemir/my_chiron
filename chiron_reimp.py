770cea91db32874f77f1d8c386c94b4e61675716
Arda Akdemir <ardaakdemir1@gmail.com>
2019-03-30 18:51:11 +0900

    test added

--- chiron_reimp.py
+++ chiron_reimp.py
@@ -12,7 +12,7 @@ from keras.layers import Dense, Activation,Input,LSTM, Lambda
 import pickle
 import sys
 import os
-
+from random import shuffle
 
 from evaluate import evaluate_preds
 
@@ -22,8 +22,47 @@ class_num = 5
 batch_size = 32
 epoch_num = 10
 seq_len = 300
+max_nuc_len = 48
 pickle_path = "toy_data.pk"
 
+def create_model(input_shape=(300,1),class_num=5,max_nuc_len =48):
+    inputs = Input(shape=input_shape)
+    input_length = Input(name='input_length', shape=[1], dtype='int64')
+    outputs = chiron_cnn(inputs,256,1,3)
+    outputs2 = chiron_rnn(outputs,200)
+    dense =  TimeDistributed(Dense(class_num))(outputs2)
+    preds = TimeDistributed(Activation('softmax',name = 'softmax'))(dense)
+    labels = Input(name='the_labels', shape=[max_nuc_len], dtype='int32')
+    input_length = Input(name='input_length', shape=[1], dtype='int32')
+    label_length = Input(name='label_length', shape=[1], dtype='int32')
+    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([preds,labels,input_length,label_length])
+    model3 = Model(inputs= [inputs,labels,input_length,label_length],outputs=loss_out)
+    return inputs,input_length,outputs,outputs2,dense,preds,labels,input_length,label_length,loss_out,model3
+
+def test_model(model_weight_path,h5_test_path, test_size = 100):
+    x_data = np.array(pickle.load(open('x_data.pk',"rb")))
+    shuffle(x_data)
+    #y_data = pickle.load(open('y_data.pk',"rb"))
+    x_data = x_data[:test_size].reshape(len(x_data[:test_size]),300,1)
+
+
+    h5_dict = read_h5("",h5_test_path,example_num = n)
+    x_tr,y_tr,y_categorical,y_labels,label_lengths = read_from_dict(h5_dict,example_num = n , class_num = 5 , seq_len = 300 ,padding = True)
+    assert len(x_tr)== len(y_tr) == len(y_categorical )== len(y_labels) == len(label_lengths), "Dimension not matched"
+    
+
+    inputs,input_length,outputs,outputs2,dense,preds,labels,input_length,label_length,loss_out,model3 = create_model()
+    flattened_input_x_width = keras.backend.squeeze(input_length, axis=-1)
+    top_k_decoded, _ = K.ctc_decode(preds, flattened_input_x_width)
+    decoder = K.function([inputs, flattened_input_x_width], [top_k_decoded[0]])
+    model3.load_weights(model_weight_path)
+    #model3.summary()
+    inputs = x_data
+    shapes = [len(x_data[0])for i in range(test_size)]
+    print(inputs.shape)
+    decoded = decoder([inputs, shapes])
+    print(decoded[0])
+    return 0
 
 ##read nucleotide sequence for each x,y pair and store in arrays
 ## pad the nucleotide sequences into max_length with 4 (denoting blank)
@@ -112,11 +151,16 @@ def ctc_predict(model,inputs,beam_width = 100, top_paths = 1):
 
 
 if __name__ == "__main__":
+    test =1
+    model_weight_path = "model3_weights.h5"
+    test_data_path = ""
+    if test == 1:
+        test_model(model_weight_path,test_data_path)
+        exit()
     args = sys.argv
     with_ctc = 1
     os.environ["CUDA_VISIBLE_DEVICES"] = '0'
     evaluate = 0
-
     h5file_path = "../../work/data/cache/train_cache.h5"
     if len(args)>1:
         h5file_path = args[1]
@@ -156,17 +200,13 @@ if __name__ == "__main__":
         label_length = Input(name='label_length', shape=[1], dtype='int32')
         loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([preds,labels,input_length,label_length])
         flattened_input_x_width = keras.backend.squeeze(input_length, axis=-1)
-
         top_k_decoded, _ = K.ctc_decode(preds, flattened_input_x_width)
         decoder = K.function([inputs, flattened_input_x_width], [top_k_decoded[0]])
-
         model3 = Model(inputs= [inputs,labels,input_length,label_length],outputs=loss_out)
         model3.summary()
         model3.compile(loss = {'ctc': lambda y_true, y_pred: y_pred},optimizer = Adam())
-
         input_lengths = np.array([300 for i in range(len(x_tr))])
         label_lengths = np.array(label_lengths)
         outputs = {'ctc': np.zeros(n)}
-
         model3.fit([x_tr,np.array(y_labels),np.array(input_lengths),np.array(label_lengths)],outputs,batch_size = batch_size,epochs=10)
         model3.save_weights("model3.h5")
